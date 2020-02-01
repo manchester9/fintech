fbe7bce119015143570d4e87cd1d77f2f8baada4
Roland Ferrao <rolandferrao@yahoo.com>
2020-01-31 19:13:51 -0500

    Add sol code for deferred equity plan

--- 16-smart_contracts_solidity/DeferredEquityPlan.sol
+++ 16-smart_contracts_solidity/DeferredEquityPlan.sol
@@ -3,18 +3,17 @@ pragma solidity ^0.5.0;
 // lvl 3: equity plan
 contract DeferredEquityPlan {
     address human_resources;
-
     address payable employee; // bob
     bool active = true; // this employee is active at the start of the contract
 
     // @TODO: Set the total shares and annual distribution
-    // Your code here!
-
+    uint total_shares = 1000;
+    uint annual_distribution = 250;
     uint start_time = now; // permanently store the time this contract was initialized
 
     // @TODO: Set the `unlock_time` to be 365 days from now
     // Your code here!
-
+    uint public unlock_time = now + 365 days;
     uint public distributed_shares; // starts at 0
 
     constructor(address payable _employee) public {
@@ -23,21 +22,21 @@ contract DeferredEquityPlan {
     }
 
     function distribute() public {
-        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
-        require(active == true, "Contract not active.");
+        require(msg.sender == human_resources || msg.sender == employee, 'You are not authorised!');
+        require(active == true, 'Contract is not active.');
 
         // @TODO: Add "require" statements to enforce that:
         // 1: `unlock_time` is less than or equal to `now`
         // 2: `distributed_shares` is less than the `total_shares`
-        // Your code here!
+        require(unlock_time <= now);
+        require(distributed_shares < total_shares);
 
         // @TODO: Add 365 days to the `unlock_time`
-        // Your code here!
-
+        unlock_time += 365 days;
+        
         // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
         // Make sure to include the parenthesis around (now - start_time) to get accurate results!
-        // Your code here!
-
+        distributed_shares = ((now - start_time) / 365 days) * annual_distribution;
         // double check in case the employee does not cash out until after 5+ years
         if (distributed_shares > 1000) {
             distributed_shares = 1000;
@@ -46,12 +45,12 @@ contract DeferredEquityPlan {
 
     // human_resources and the employee can deactivate this contract at-will
     function deactivate() public {
-        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to deactivate this contract.");
+        require(msg.sender == human_resources || msg.sender == employee, 'You are not authorised!');
         active = false;
     }
 
     // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
     function() external payable {
-        revert("Do not send Ether to this contract!");
+        revert('Wrong contract to send Ether!');
     }
 }
